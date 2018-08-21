a203f0a31c1c144854b3e16cb14738e539f59b35
Rosie Bahrani <rosie.bahrani@uwaterloo.ca>
2018-08-20 21:41:42 -0400

    minor changes. Libraries added

--- mnist_data_creation.py
+++ mnist_data_creation.py
@@ -1,11 +1,18 @@
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 batch_xs, batch_ys = mnist.train.next_batch(100)
 
-data = batch_xs[0]
-label = batch_ys[0]
+data = np.rint(batch_xs[0]).astype(int)
+label = np.rint(batch_ys[0]).astype(int)
+pixels = ata.reshape((28,28))
 
 print(data)
 print(label)
+
+plt.imshow(pixels, cmap="grey")
+plt.show()
